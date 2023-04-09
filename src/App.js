57ea02f50cc6cf001324e3d050a86b66c257f28b
aravindra prasad <aravindraprasad16@gmail.com>
2023-04-09 09:55:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [Hori,setHori] = useState(10) 
+  const [Veri,setVeri] = useState(10) 
+  const [Blur,setBlur] = useState(10) 
+  const [color,setColor] = useState("black") 
+  const [bcolor,setbColor] = useState("blue") 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className='App'>
+        <div className="controls">
+          <label> Horizontal length </label>
+          <input type="range"  min="-200" max="200" value={Hori} onChange={(e) => setHori(e.target.value)} />
+          <label> Vertical length </label>
+          <input type="range"  min="-200" max="200" value={Veri} onChange={(e) => setVeri(e.target.value)} />
+          <label> Blur </label>
+          <input type="range"  min="0" max="200" value={Blur} onChange={(e) => setBlur(e.target.value)} />
+          <label> Color </label>
+          <input type="color"   value={color} onChange={(e) => setColor(e.target.value)} />
+          <label> Background-Color </label>
+          <input type="color"   value={bcolor} onChange={(e) => setbColor(e.target.value)} />
+        </div>
+
+        <div className="output">
+          
+          <div className="box" style={{boxShadow:`${Hori}px ${Veri}px ${Blur}px ${color}`, background:`${bcolor}` }}>
+           <p>  Box-shadow:{Hori}px {Veri}px {Blur}px {color}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 
 export default App;
